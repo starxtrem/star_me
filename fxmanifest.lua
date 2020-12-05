5d0e8444eb26b90694a05e12bb7084d30d8314eb
starxtrem <83julcraft@gmail.com>
2020-12-06 00:50:03 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,6 +3,7 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 fx_version 'adamant'
 game 'gta5'
 
+author 'Starxtrem'
 description 'ME Fait par starxtrem'
 
 client_script 'client.lua'
