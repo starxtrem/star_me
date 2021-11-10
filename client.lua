b032743a70642edc70ed29454e92bd521aee0c4a
Starxtrem <59668040+starxtrem@users.noreply.github.com>
2021-11-10 08:33:20 -0500

    Update client.lua

--- client.lua
+++ client.lua
@@ -20,48 +20,48 @@ end)
 
 local pedDisplaying = {}
 local function DrawText3D(coords, text)
-local camCoords = GetGameplayCamCoord()
-local dist = #(coords - camCoords)
-local scale = 200 / (GetGameplayCamFov() * dist)
-if text == "*La personne x *" or text == "*La personne  x *" then
-	return
-else
-	SetTextColour(color.r, color.g, color.b, color.a)
-	SetTextScale(0.0, defaultScale * scale)
-	SetTextDropshadow(0, 0, 0, 0, 55)
-	SetTextDropShadow()
-	SetTextCentre(true)
-	BeginTextCommandDisplayText("STRING")
-	AddTextComponentSubstringPlayerName(text)
-	SetDrawOrigin(coords, 0)
-	EndTextCommandDisplayText(0.0, 0.0)
-	ClearDrawOrigin()
+	local camCoords = GetGameplayCamCoord()
+	local dist = #(coords - camCoords)
+	local scale = 200 / (GetGameplayCamFov() * dist)
+	if text == "*La personne x *" or text == "*La personne  x *" then
+		return
+	else
+		SetTextColour(color.r, color.g, color.b, color.a)
+		SetTextScale(0.0, defaultScale * scale)
+		SetTextDropshadow(0, 0, 0, 0, 55)
+		SetTextDropShadow()
+		SetTextCentre(true)
+		BeginTextCommandDisplayText("STRING")
+		AddTextComponentSubstringPlayerName(text)
+		SetDrawOrigin(coords, 0)
+		EndTextCommandDisplayText(0.0, 0.0)
+		ClearDrawOrigin()
 	end
 end
 
 local function Display(ped, text, iddebase)
-local playerPed = PlayerPedId()
-local playerCoords = GetEntityCoords(playerPed)
-local pedCoords = GetEntityCoords(ped)
-local dist = #(playerCoords - pedCoords)
-if pedCoords ~= playerCoords or iddebase == idserver then
-	if dist <= distToDraw then
-		pedDisplaying[ped] = (pedDisplaying[ped] or 1) + 1
-		local display = true
-		Citizen.CreateThread(function()
-			Wait(displayTime)
-			display = false
-		end)
-		local offset = 0.8 + pedDisplaying[ped] * 0.1
-		while display do
-			if HasEntityClearLosToEntity(playerPed, ped, 17 ) then
-				local x, y, z = table.unpack(GetEntityCoords(ped))
-				z = z + offset
-				DrawText3D(vector3(x, y, z), text)
+	local playerPed = PlayerPedId()
+	local playerCoords = GetEntityCoords(playerPed)
+	local pedCoords = GetEntityCoords(ped)
+	local dist = #(playerCoords - pedCoords)
+	if pedCoords ~= playerCoords or iddebase == idserver then
+		if dist <= distToDraw then
+			pedDisplaying[ped] = (pedDisplaying[ped] or 1) + 1
+			local display = true
+			Citizen.CreateThread(function()
+				Wait(displayTime)
+				display = false
+			end)
+			local offset = 0.8 + pedDisplaying[ped] * 0.1
+			while display do
+				if HasEntityClearLosToEntity(playerPed, ped, 17 ) then
+					local x, y, z = table.unpack(GetEntityCoords(ped))
+					z = z + offset
+					DrawText3D(vector3(x, y, z), text)
+				end
+				Wait(0)
 			end
-			Wait(0)
-		end
-		pedDisplaying[ped] = pedDisplaying[ped] - 1
+			pedDisplaying[ped] = pedDisplaying[ped] - 1
 		end
 	end
 end
